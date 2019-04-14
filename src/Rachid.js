18b80210aa38f2148b989a5c099d947030ce771d
Marcos Rachid <marcosrachid@gmail.com>
2019-04-14 10:42:41 -0300

    img and css

--- src/Rachid.js
+++ src/Rachid.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import User from './User';
+import photo from './photo.png';
 
 class Rachid extends Component {
   render() {
     return (
       <div>
-        Whatever
+        <User name="Marcos Rachid" photo={photo}></User>
+        <User name="João Rachid" photo={photo}></User>
+        <User name="Pedro Rachid" photo={photo}></User>
       </div>
     );
   }
